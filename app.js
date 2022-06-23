9ffcbeda4d9b274e4a54309627b3fa42ba4863e7
saidugurlu <ugurlumsaid@gmx.de>
2022-06-23 22:02:11 +0200

    data came from mongo dv

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+const Photo = require("./models/Photo");
+
 
 const app = express();
+
+//connect DB
+mongoose.connect("mongodb://localhost/photo-memories");
+
 const port = 3011;
 
 //Template Engine
@@ -10,13 +17,13 @@ app.set("view engine", "ejs");
 
 // Middlewares
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 //Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async(req, res) => {
+  const photos = await Photo.find({})
+  res.render("index", { photos});
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -24,8 +31,8 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
   res.render("add");
 });
-app.post("/photos", (req, res) => {
-  console.log(req.body);
+app.post("/photos", async(req, res) => {
+await Photo.create(req.body)
   res.redirect("/");
 });
 
