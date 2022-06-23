a4a6c7f475ff6ec553ba900aa5b2ae65b1b3d81a
saidugurlu <ugurlumsaid@gmx.de>
2022-06-23 14:36:55 +0200

    Template Engine added

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
+const app = express();
+const port = 3011;
 
-const port = 3011
+//Template Engine
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+// Middlewares
+app.use(express.static("public"));
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,"temp/index.html"));
+//Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
