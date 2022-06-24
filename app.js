40c7726bf7d9da89df1f216870ec47a5f93165f1
saidugurlu <ugurlumsaid@gmx.de>
2022-06-24 15:03:07 +0200

    photo detail page work

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const ejs = require("ejs");
 const path = require("path");
 const Photo = require("./models/Photo");
 
-
 const app = express();
 
 //connect DB
@@ -21,18 +20,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-app.get("/", async(req, res) => {
-  const photos = await Photo.find({})
-  res.render("index", { photos});
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", { photos });
 });
+
+app.get("/photos/:id", async(req, res) => {
+ const photo =  await Photo.findById(req.params.id);
+ res.render("photo", { photo})
+});
+
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
 app.get("/add", (req, res) => {
   res.render("add");
 });
-app.post("/photos", async(req, res) => {
-await Photo.create(req.body)
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
   res.redirect("/");
 });
 
