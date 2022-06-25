583627ec4c9299fc0582593f6f7c8f3050612369
saidugurlu <ugurlumsaid@gmx.de>
2022-06-25 13:51:11 +0200

    bud edit title:can not change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 const fileUpload = require("express-fileupload");
 const ejs = require("ejs");
 const path = require("path");
@@ -12,7 +12,6 @@ const app = express();
 //connect DB
 mongoose.connect("mongodb://localhost/photo-memories");
 
-
 //Template Engine
 app.set("view engine", "ejs");
 
@@ -21,7 +20,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'))
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 //Routes
 app.get("/", async (req, res) => {
@@ -67,17 +70,24 @@ app.post("/photos", async (req, res) => {
 });
 
 app.get("/photos/edit/:id", async (req, res) => {
-  const photo = await Photo.findOne({_id:req.params.id})
-  res.render("edit", { photo});
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render("edit", { photo });
 });
 
-app.put("/photos/:id", async(req, res) => {
-  const photo = await Photo.findOne({_id:req.params.id})
-  photo.description = req.body.description
-  photo.save()
+app.put("/photos/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.description = req.body.description;
+  photo.save();
 
-  res.redirect(`/photos/${req.params.id}`)
+  res.redirect(`/photos/${req.params.id}`);
+});
 
+app.delete("/photos/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + "/public" + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect("/");
 });
 
 const port = 3011;
